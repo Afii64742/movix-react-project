4b8ea0c62a8b9e1de89828b604015f298f29626e
Aftab <afii64742@gmail.com>
2024-03-30 22:07:07 +0500

    Responsive animated header with search box is created + header show / hide on scroll

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,13 @@ import SearchResult from "./pages/searchResult/SearchResult";
 import Explore from "./pages/explore/Explore";
 import PageNotFound from "./pages/404/PageNotFound";
 import Detail from "./pages/detail/Detail";
+
+// COMPONENTS IMPORTS
+
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+
+// fetchDataFromApi function
 import { fetchDataFromApi } from "./utils/api";
 
 function App() {
@@ -40,15 +47,19 @@ function App() {
   }, []);
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:mediaType/:id" element={<Detail />} />
-        <Route path="/search/:query" element={<SearchResult />} />
-        <Route path="/explore/:mediaType" element={<Explore />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:mediaType/:id" element={<Detail />} />
+          <Route path="/search/:query" element={<SearchResult />} />
+          <Route path="/explore/:mediaType" element={<Explore />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+      {/* <Footer /> */}
+    </>
   );
 }
 
--- src/pages/home/banner/Banner.jsx
+++ src/pages/home/banner/Banner.jsx
@@ -4,15 +4,16 @@ import Img from "../../../components/lazyLoadImage/Img";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import useFetch from "../../../hooks/useFetch";
 import { useSelector } from "react-redux";
+// import useNavigate hook from react router dom redirect on routes
+import { useNavigate } from "react-router-dom";
+// instance of useNavigate Hook
+
 const Banner = () => {
+  const navigate = useNavigate();
   const [background, setBackground] = useState("");
   const [query, setQuery] = useState("");
   const { url } = useSelector((state) => state.home);
   const { data, loading } = useFetch("/movie/upcoming");
-  console.log(data);
-  const searchQueryHandler = () => {
-    console.log("query handler function");
-  };
 
   useEffect(() => {
     const bg =
@@ -20,6 +21,12 @@ const Banner = () => {
       data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
     setBackground(bg);
   }, [data]);
+
+  const searchQueryHandler = (event) => {
+    if (event.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+    }
+  };
 
   return (
     <div className="heroBanner">
@@ -39,7 +46,7 @@ const Banner = () => {
             <input
               type="text"
               onKeyUp={searchQueryHandler}
-              onChange={(e) => e.target.value}
+              onChange={(e) => setQuery(e.target.value)}
             />
             <button>Search</button>
           </div>
