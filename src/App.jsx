5a8172999bb931c31b7bf54525baca4316486c5b
Aftab <afii64742@gmail.com>
2024-03-30 22:30:00 +0500

    Footer is created

--- src/App.jsx
+++ src/App.jsx
@@ -57,8 +57,8 @@ function App() {
           <Route path="/explore/:mediaType" element={<Explore />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
-      {/* <Footer /> */}
     </>
   );
 }
